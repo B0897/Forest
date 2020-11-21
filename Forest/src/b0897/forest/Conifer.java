66f079c926b204c5687a0ef3ea24483d5eed2582
User <User@LAPTOP-MB3D3UV6.RT-AC1300G>
2020-11-21 16:12:18 +0100

    new Class Conifer

---
+++ Forest/src/b0897/forest/Conifer.java
@@ -0,0 +1,52 @@
+package b0897.forest;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+public class Conifer extends Tree{
+	
+	private int squirrels;
+
+	public Conifer(){
+	}
+
+	public Conifer(Tree copied) {
+		super(copied);
+		this.squirrels = 0;
+	}
+
+	public Conifer(String name, int height, int rings, boolean lonely, int nr, int broken, String shape, int nr2, int sqrl) {
+		super(name, height, rings, lonely, nr, broken, shape, nr2);
+		this.squirrels = sqrl;
+	}
+	
+	@Override
+	public void grow() {
+		this.setHeight(this.getHeight()* 1.1);
+		this.setRings(this.getRings()+1);
+	}
+	
+	public void choinka() {		
+		Desktop desk = Desktop.getDesktop();
+		try {
+			desk.open(new File("plik.mp3"));
+		} catch (IOException e) {
+			System.out.println("did not work out :<");
+			e.printStackTrace();
+		} finally {
+			System.out.println("\n * * * HO * * * HO * * * HOOO * * *");
+		}
+	}
+
+
+
+	public int getSquirrels() {
+		return squirrels;
+	}
+	
+	public void setSquirrels(int squirrels) {
+		this.squirrels = squirrels;
+	}
+
+}
